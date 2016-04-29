b9f52fbcfeac8c6aa46098c5b6df1c77c71aa5d5
Florian Breisch <florian.breisch@mindkeeper.solutions>
2016-04-29 15:08:32 +0200

    initial prerelease

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+import meow from 'meow'
+
 // import CliApp from 'cli/main'
 // import GuiApp from 'gui/main'
 
 class App {
 	static run() {
+		const commandName = 'dimchi'
+		const cli = meow(`
+			Usage:
+				$ ${commandName} <options>
+`)
+		cli.showHelp()
 		console.log('running now')
 		for (const argument of process.argv) {
 			console.log('found argument: ', argument)
